524990bc6a9244974c83e7ddf2b866683a289fcb
Richard Hua <richardhua@v1040-wn-rt-a-5-226.campus-dynamic.uwaterloo.ca>
2023-09-11 22:16:40 -0400

    Responsive Design Fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='Container'>
+    <div className="Container">
         <Board />
     </div>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Row.css'
 
 function Row({k}) {
+    const r = {
+        height: '10vh',
+        width: '10vh'
+    }
+    const t = {
+        height: '10vw',
+        width: '10vw'
+    }
+    const [val, setVal] = useState(r);
+    const handleResize = () => {
+        if (window.innerWidth > window.innerHeight) {
+            setVal(r)
+        } else {
+            setVal(t)
+        }
+        
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+            return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     return (
         <div>
             {k.map((m) => (
-                <div className="box">
+                <div className="box" style = {val}>
                     {m.id}
                 </div>
             ))}
