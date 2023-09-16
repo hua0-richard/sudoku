00d3d41ef942086d165d21a67bdeedf51b4b2a4c
Richard Hua <hua.richard0@gmail.com>
2023-09-16 13:58:20 -0400

    Helper Functions

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -3,12 +3,12 @@ import "./Sudoku.css";
 
 import Box from "./Box";
 
-function boxData() {
+function boxData(a, b) {
   let box = [];
   for (let i = 0; i < 3; i++) {
     let row = [];
     for (let j = 0; j < 3; j++) {
-      let item = { id: i.toString() + " " + j.toString(), input:  true};
+      let item = { id: a.toString() + " " + b.toString(), input:  true};
       row.push(item);
     }
     box.push(row);
@@ -21,11 +21,31 @@ function sudokuData() {
   for (let i = 0; i < 3; i++) {
     let row = [];
     for (let j = 0; j < 3; j++) {
-      row.push(boxData());
+      row.push(boxData(i, j));
     }
     sudoku.push(row);
   }
+  console.log(flattenSudoku(sudoku));
   return sudoku; 
+}
+
+function flattenSudoku(s) {
+  let flat = []; 
+  console.log(s)
+  s.forEach(t => {
+    t.forEach(u => {
+      u.forEach(v => {
+        v.forEach(w => {
+          flat.push(w);
+        })
+      })
+    });
+  });
+  return flat; 
+}
+
+function copyIntoSudoku() {
+
 }
 
 function Sudoku() {
